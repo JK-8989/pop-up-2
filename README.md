1147ad21bedccd542eff067f8c92f6c31addf615
JK-8989 <jiyoungkim365@gmail.com>
2022-05-24 13:28:05 -0700

    Edit script btn fuctions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pop-up-2
+
+pop-up window using JavaScript
+
+reference: Nomad Coders
+https://www.youtube.com/watch?v=V08wXKHF_Xw&t=374s
---
+++ script.js
@@ -0,0 +1,14 @@
+const openBtn = document.getElementById('open');
+const modal = document.querySelector('.modal');
+const openModal = () => {
+  modal.classList.remove('hidden');
+}
+openBtn.addEventListener('click', openModal);
+
+const closeBtn = document.querySelector('#close')
+const bg = document.querySelector('.modal__overlay')
+const closeModal = () => {
+  modal.classList.add('hidden')
+}
+closeBtn.addEventListener('click', closeModal)
+bg.addEventListener('click', closeModal)
